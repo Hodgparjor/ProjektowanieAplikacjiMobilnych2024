392ca0f63b38892fed236ac411c7b92fca9e79df
AleksanderBrzostowski <olekbrzost@gmail.com>
2024-04-11 21:31:38 +0200

    First commit. WIP Calculator GUI

---
+++ Calculator_PAM/app/src/main/java/com/example/calculator_pam/AdvancedCalculatorActivity.java
@@ -0,0 +1,4 @@
+package com.example.calculator_pam;
+
+public class AdvancedCalculatorActivity {
+}
---
+++ Calculator_PAM/app/src/main/java/com/example/calculator_pam/SimpleCalculatorActivity.java
@@ -0,0 +1,6 @@
+package com.example.calculator_pam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SimpleCalculatorActivity extends AppCompatActivity {
+}
